7a81be4ad00aa1c2c1c67ff260a58bed30322ad7
José Miguel <MontevaniOthon@gmail.com>
2021-02-24 20:41:44 -0300

    Configurando migrations
    up e down configurados

--- src/database/migrations/1614209643206-CreateUsers.ts
+++ src/database/migrations/1614209643206-CreateUsers.ts
@@ -1,11 +1,39 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUsers1614209643206 implements MigrationInterface {
 
+    //Cria
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar", //string, não pode ser nulo
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()", //O próprio banco de dados irá se encarregar de atribuir a data de criação
+                    }
+                ]
+            })
+        )
     }
 
+    //Destrói
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
     }
 
 }
