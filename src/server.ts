35af87396cf948d9ccf9444ed6218fd4de9f0796
José Miguel <MontevaniOthon@gmail.com>
2021-02-24 21:38:51 -0300

    Controladores e Servidor
    Criar a estrutura para se fazer o cadastro. Para o servidor não ficar poluído será utilizado um controlador com as funções.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import 'reflect-metadata';
 import express from 'express';
-
 const app = express();
-
 import "./database"; //reconhece por padrão que o arquivo que precisa ser importado é o index
-
+import { router } from "./routes";
 
 /**
  * GET => Busca
@@ -13,20 +11,8 @@ import "./database"; //reconhece por padrão que o arquivo que precisa ser impor
  * Delete => Deletar
  * Patch => Alteração específica
  */
+//http://localhost:3333/users
 
-    //http://localhost:3333/users
-
-app.get("/", (resquest, response) => {
-    return response.json({message:"Hello, Potion Seller. I need your strongest potions."});
-});
-
-// 1º param => Rota (recurso API)
-// 2º param => request, response
-
-app.post("/", (request, response) => {
-    return response.json({message: "Os dados foram salvos com sucesso!"});
-});
-
-//As rotas podem ser iguais mas os métodos diferentes
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
