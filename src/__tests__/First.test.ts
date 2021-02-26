5ad5b044e375ebaab4a221ed6086e4a4476e7545
José Miguel <MontevaniOthon@gmail.com>
2021-02-25 23:31:02 -0300

    inicializando testes
    Começando a implementar as funções de teste. No momento estou criando um banco de dados secundário para testar users.

---
+++ src/__tests__/First.test.ts
@@ -0,0 +1,9 @@
+describe("First", () => {
+    it("Deve ser possível somar 2 números.", () => {
+        expect(2+2).toBe(4);
+    });
+
+    it("Deve ser impossível conseguir um resultado diferente de 4.", () => {
+        expect(2+2).not.toBe(4);
+    });
+});
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,3 @@
-import 'reflect-metadata';
-import express from 'express';
-const app = express();
-import "./database"; //reconhece por padrão que o arquivo que precisa ser importado é o index
-import { router } from "./routes";
-
-/**
- * GET => Busca
- * Post => Salvar
- * Put => Alterar
- * Delete => Deletar
- * Patch => Alteração específica
- */
-//http://localhost:3333/users
-
-app.use(express.json());
-app.use(router);
+import { app } from "./app";
 
 app.listen(3333, () => console.log("Server is running!"));
