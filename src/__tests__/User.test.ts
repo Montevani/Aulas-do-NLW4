4906660beba2129476c1f2a6976fdbb32e44b76c
José Miguel <MontevaniOthon@gmail.com>
2021-02-27 18:34:37 -0300

    Migration de SurveysUsers

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -16,5 +16,16 @@ describe("Users", () => {
         });
     
         expect(response.status).toBe(201);
-    })
+    });
+
+    it("Should not be able to create an user with an existing email.", async () => {
+        const response = await request(app).post("/users").send({
+            email: "user@example.com",
+            name: "User Example",
+        });
+
+        expect(response.status).toBe(400);
+    });
 });
+
+//"posttest": "rm ./src/database/database.test.sqlite"
